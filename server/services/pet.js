7b9bf4759a8a74b58e51f06556cad3059ee4cbdd
facutorraca <ftorraca@fi.uba.ar>
2021-06-21 16:56:14 -0300

    fix merge

--- server/services/pet.js
+++ server/services/pet.js
@@ -28,11 +28,7 @@ class PetService {
       const pet = await Pet.create(petDTO);
       return pet;
     } catch (err) {
-<<<<<<< HEAD
-      console.log(err)
-=======
       console.log(err);
->>>>>>> 9206c0b1a41a81c62f7783f4077e0347e444c46d
     }
   }
 }
