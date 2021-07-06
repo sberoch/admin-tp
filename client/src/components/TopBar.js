416feaf7efa48e3e3df9a0afe46daff2532e9fe8
lbellaera <lbellaera@fi.uba.ar>
2021-07-06 18:42:30 -0300

    adds logout button

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -7,22 +7,31 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 import { useHistory } from "react-router";
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
-
-function HomeIcon(props) {
-  return (
-    <SvgIcon {...props}>
-      <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-    </SvgIcon>
-  );
-}
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
+import { useAuth } from '../contexts/AuthContext'
 
 export default function TopBar() {
+  const { logout } = useAuth();
+
+  function HomeIcon(props) {
+    return (
+      <SvgIcon {...props}>
+        <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+      </SvgIcon>
+    );
+  }
+
   const history = useHistory();
+
+  const handleLogout = async () => {
+    await logout();
+    history.push('');
+  }
 
   const isInHome = (history.location.pathname === '/home');
   let res;
   if (!isInHome) {
-    res = <Button variant="contained" color="primary" onClick={() => history.push('/home')}>
+    res = <Button variant="outlined" color="primary" onClick={() => history.push('/home')}>
           <HomeIcon style={{ color: "green", fontSize: 40}}/>
           </Button>;
   }
@@ -33,10 +42,10 @@ export default function TopBar() {
         <Box display='flex' flexGrow={1}>
           {res}
         </Box>
-        <img src={logo} alt="Logo" style={{height: 50, width: 50, marginLeft: 10}}/>
-        <Typography variant="h6" color="inherit" noWrap>
-          Rescue Me
-        </Typography>
+        <img src={logo} alt="Logo" style={{height: 50, width: 50}}/>
+        <Button onClick={handleLogout}>
+          <MeetingRoomIcon size="large"/>
+        </Button>
       </Toolbar>
     </AppBar> 
   );
